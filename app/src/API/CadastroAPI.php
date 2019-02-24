4527b2404782572dac6d85ea9b9b50f7615c7197
Gelson Junior <junior.ppp@gmail.com>
2019-02-24 00:30:32 -0300

    atualização CadastroAPI

--- app/src/API/CadastroAPI.php
+++ app/src/API/CadastroAPI.php
@@ -14,17 +14,37 @@ final class CadastroAPI
     {
         $this->pdo = $pdo;
     }
+	
+	private function SalvaChave(array $Parametros ){		
+		$SalvaChave = $this->pdo->prepare("insert into chave (serial, validade, status, idcliente, mac_address, qrcode) values (:chave,:validade,:status,:idcliente,:mac,:qrcode)");
+		$SalvaChave->execute($Parametros);	
+		return $this->pdo->lastInsertId();
+	}
 
     public function __invoke(Request $request, Response $response, $args)
     {
-		array_push($_POST, array("data"=>"2000-01-01","status"=>"1") );
-		unset($_POST['chave']);
-		unset($_POST['mac_address']);
-		$dados = $this->pdo->query("insert into cliente (nome, data_nasc, cpf, email, telefone, senha, status, data_cadastro) values (:nome, :nasc, :cpf, :email, :telefone, :senha, :status, :data)");
-		$dados->execute($_POST);
-
-		$newResponse = $response->withStatus(200)->withJson(['status' => "200", "mensagem" => "Usuario cadastrado com sucesso", "validade" => "2019-03-11"]);
+		//ARMAZENANDO DADOS
+		$Chave = $_POST["chave"];
+		$Mac = $_POST["mac_address"];
 		
+		$Complemento = array("data"=>date("Y-m-d"),"status"=>"1");
+		$Teste = array_merge($_POST, $Complemento);
+		
+		unset($Teste['chave']);
+		unset($Teste['mac_address']);
+
+		$dados = $this->pdo->prepare("insert into cliente (nome, data_nasc, cpf, email, telefone, senha, status, data_cadastro) values (:nome, :nasc, :cpf, :email, :telefone, :senha, :status, :data)");
+		$dados->execute($Teste);
+		
+		$IDCliente = $this->pdo->lastInsertId();
+		
+		$ValidadeChave = date('Y-m-d', strtotime("+1 days",strtotime(date("Y-m-d"))));
+		$Keys = array("chave"=>$Chave,"validade"=>$ValidadeChave,"status"=>"1","idcliente"=>$IDCliente,"mac"=>$Mac,"qrcode"=>"");
+		
+		$IDChave = self::SalvaChave($Keys);
+		$StatusResposta = $IDChave ? "200" : "400";
+		
+		$newResponse = $response->withStatus(200)->withJson(['status' => $StatusResposta, "mensagem" => "Usuario cadastrado com sucesso".$IDChave, "validade" => $ValidadeChave, "id"=>$IDCliente, "idchave"=>$IDChave]);
 		
 		return $newResponse;
 			
